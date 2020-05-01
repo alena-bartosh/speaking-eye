edacfaef7f21e183d73caca8c73f526f585a23f6
Alena Bartosh <alena.mathematics@gmail.com>
2020-05-01 23:48:17 +0300

    :recycle: Add fallback tray icon
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>
    
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>

--- src/tray_icon.py
+++ src/tray_icon.py
@@ -1,11 +1,13 @@
 from gi.repository import AppIndicator3 as AppIndicator
 
+FALLBACK_ICON = 'face-monkey'
+
 
 class TrayIcon:
     def __init__(self, app_id, icon, menu):
         self.menu = menu
 
-        self.ind = AppIndicator.Indicator.new(app_id, '', AppIndicator.IndicatorCategory.APPLICATION_STATUS)
+        self.ind = AppIndicator.Indicator.new(app_id, FALLBACK_ICON, AppIndicator.IndicatorCategory.APPLICATION_STATUS)
         self.ind.set_icon(icon)
         self.ind.set_status(AppIndicator.IndicatorStatus.ACTIVE)
         self.ind.set_menu(self.menu)
