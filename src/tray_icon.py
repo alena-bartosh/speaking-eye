c267e4120690c8a38b50c64d3aaa9eff7cb2d8a6
Alena Bartosh <alena.mathematics@gmail.com>
2020-05-02 00:53:46 +0300

    :recycle: Rename ind to indicator
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>

--- src/tray_icon.py
+++ src/tray_icon.py
@@ -9,8 +9,9 @@ class TrayIcon:
     def __init__(self, app_id: str, icon: str, menu: Gtk.Menu):
         self.menu = menu
 
-        self.ind = AppIndicator.Indicator.new(app_id, FALLBACK_ICON, AppIndicator.IndicatorCategory.APPLICATION_STATUS)
+        self.indicator = \
+            AppIndicator.Indicator.new(app_id, FALLBACK_ICON, AppIndicator.IndicatorCategory.APPLICATION_STATUS)
         if os.path.exists(icon):
-            self.ind.set_icon(icon)
-        self.ind.set_status(AppIndicator.IndicatorStatus.ACTIVE)
-        self.ind.set_menu(self.menu)
+            self.indicator.set_icon(icon)
+        self.indicator.set_status(AppIndicator.IndicatorStatus.ACTIVE)
+        self.indicator.set_menu(self.menu)
