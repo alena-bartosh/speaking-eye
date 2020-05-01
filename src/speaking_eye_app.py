444dde686f358c9ba096bdf5b927a49ca39d624e
Alena Bartosh <alena.mathematics@gmail.com>
2020-05-01 17:16:01 +0300

    :sparkles: Print window title changes
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>

--- src/speaking_eye_app.py
+++ src/speaking_eye_app.py
@@ -9,6 +9,7 @@ class SpeakingEyeApp(Gtk.Application):
         super().__init__()
         self.screen = None
         self.main_loop = None
+        self.name_changed_handler_id = None
 
     def do_activate(self) -> None:
         self.screen = Wnck.Screen.get_default()
@@ -18,7 +19,17 @@ class SpeakingEyeApp(Gtk.Application):
     def on_active_window_changed(self, screen: Wnck.Screen, previously_active_window: Gtk.Window) -> None:
         active_window = screen.get_active_window()
 
+        # to prevent double handler connections
+        if self.name_changed_handler_id:
+            previously_active_window.disconnect(self.name_changed_handler_id)
+
+        if active_window:
+            self.name_changed_handler_id = active_window.connect('name-changed', self.on_name_changed)
+
         print(f'{get_window_name(previously_active_window)} -> {get_window_name(active_window)}')
+
+    def on_name_changed(self, window: Wnck.Window) -> None:
+        print(f'\t new name: {get_window_name(window)}')
 
     def start_main_loop(self) -> None:
         try:
