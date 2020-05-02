67bc91167d39b2b37980149d1e0cf046057db80d
Alena Bartosh <alena.mathematics@gmail.com>
2020-05-02 15:38:31 +0300

    :construction: Print work time for every app
    * Show notifications
    
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import gi
 gi.require_version('Wnck', '3.0')
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
+gi.require_version('Notify', '0.7')
 
 import sys
 
--- src/speaking_eye_app.py
+++ src/speaking_eye_app.py
@@ -1,6 +1,8 @@
-import os
 from datetime import datetime, timedelta
-from gi.repository import Wnck, Gtk, GObject
+import json
+import os
+
+from gi.repository import Wnck, Gtk, GObject, Notify
 
 from gtk_extras import get_window_name
 from tray_icon import TrayIcon
@@ -18,10 +20,22 @@ class SpeakingEyeApp(Gtk.Application):
         self.icon = TrayIcon(APP_ID, ACTIVE_ICON, self.create_tray_menu())
         self.screen = None
         self.main_loop = None
+        self.notify = Notify
         self.name_changed_handler_id = None
         self.start_time = datetime.now()
+        self.active_window_start_time = datetime.now()
+        self.wm_class = ''
 
-        print(f'### Start time: [{self.start_time.strftime("%Y-%m-%d %H:%M:%S")}]')
+        self.active_tab_start_time = datetime.now()
+        self.active_tab_work_time = 0
+
+        self.apps_time = {}
+
+        self.notify.init(APP_ID)
+
+        start_msg = f'### Start time: [{self.start_time.strftime("%Y-%m-%d %H:%M:%S")}]'
+        print(start_msg)
+        self.show_notification(msg=start_msg)
 
     def do_activate(self) -> None:
         self.screen = Wnck.Screen.get_default()
@@ -29,7 +43,17 @@ class SpeakingEyeApp(Gtk.Application):
         self.main_loop = GObject.MainLoop()
 
     def on_active_window_changed(self, screen: Wnck.Screen, previously_active_window: Gtk.Window) -> None:
-        wm_class = ''
+        active_window_work_time = datetime.now() - self.active_window_start_time
+
+        print(f'CLOSE {self.wm_class} [{active_window_work_time}]')
+
+        if self.wm_class in self.apps_time:
+            self.apps_time[self.wm_class] += active_window_work_time
+        else:
+            self.apps_time[self.wm_class] = active_window_work_time
+
+        self.active_window_start_time = datetime.now()
+
         active_window = screen.get_active_window()
 
         # to prevent double handler connections
@@ -38,32 +62,41 @@ class SpeakingEyeApp(Gtk.Application):
 
         if active_window:
             self.name_changed_handler_id = active_window.connect('name-changed', self.on_name_changed)
-            wm_class = get_wm_class(active_window.get_xid())
+            self.wm_class = get_wm_class(active_window.get_xid())
 
-        print(f'{wm_class}')
+        print(f'OPEN {self.wm_class}')
         print(f'\t{get_window_name(active_window)}')
 
     def on_name_changed(self, window: Wnck.Window) -> None:
-        print(f'\t-> {get_window_name(window)}')
+        self.active_tab_work_time = round((datetime.now() - self.active_tab_start_time).total_seconds(), 3)
+        print(f'\t[{self.active_tab_work_time}] -> {get_window_name(window)}')
+        self.active_tab_start_time = datetime.now()
 
     def start_main_loop(self) -> None:
         try:
             self.main_loop.run()
         except KeyboardInterrupt:
-            self.stop_main_loop()
+            self.stop()
 
-    def stop_main_loop(self) -> None:
+    def stop(self) -> None:
         finish_time = datetime.now()
-        print(f'### Finish time: [{finish_time.strftime("%Y-%m-%d %H:%M:%S")}]')
-
         work_time = finish_time - self.start_time
         work_time -= timedelta(microseconds=work_time.microseconds)
-        print(f'### Work time: [{work_time}]')
+
+        finish_msg = f'### Finish time: [{finish_time.strftime("%Y-%m-%d %H:%M:%S")}]'
+        work_time_msg = f'### Work time: [{work_time}]'
+
+        print(f'{finish_msg}\n{work_time_msg}')
+        print(f'### Apps time: {json.dumps(self.apps_time, indent=2, default=str)}')
+
+        self.show_notification(msg=finish_msg)
+        self.show_notification(msg=work_time_msg)
+        self.notify.uninit()
 
         self.main_loop.quit()
 
     def on_close_item_click(self, menu_item: Gtk.MenuItem) -> None:
-        self.stop_main_loop()
+        self.stop()
 
     def create_tray_menu(self) -> Gtk.Menu:
         menu = Gtk.Menu()
@@ -75,3 +108,6 @@ class SpeakingEyeApp(Gtk.Application):
         menu.show_all()
 
         return menu
+
+    def show_notification(self, msg: str) -> None:
+        self.notify.Notification.new(f'|SPEAKING EYE| {msg}').show()
