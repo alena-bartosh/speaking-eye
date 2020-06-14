9d1abe8ddba147450afd1092385c40d2ea2aa28e
Alena Bartosh <alena.mathematics@gmail.com>
2020-06-14 23:54:44 +0300

    🚧 Override __eq__ in ApplicationInfo
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>

--- src/application_info.py
+++ src/application_info.py
@@ -1,6 +1,25 @@
+# TODO: add unittests
+
+
 class ApplicationInfo:
 
     def __init__(self, name: str, wm_name: str, tab: str):
         self.name = name
         self.wm_name = wm_name
         self.tab = tab
+
+    def __eq__(self, other):
+        """
+        Overrides the default implementation
+        to use the object values instead of identifiers for comparison
+        """
+        if not isinstance(other, ApplicationInfo):
+            return False
+
+        if self.name != other.name:
+            return False
+
+        if self.wm_name != other.wm_name:
+            return False
+
+        return self.tab == other.tab
