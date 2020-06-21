6640958cb9c5b200105163e4c5927c7aaa0faf74
Alena Bartosh <alena.mathematics@gmail.com>
2020-06-21 16:29:55 +0300

    ♻️ ApplicationInfoReader: extract handling logic methods
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>

--- src/application_info_reader.py
+++ src/application_info_reader.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import List
+from typing import Dict, List
 
 from application_info import ApplicationInfo
 
@@ -18,6 +18,21 @@ class SpecialApplicationInfo(Enum):
 
 class ApplicationInfoReader:
 
+    def __handle_special_case(self, special_case: SpecialApplicationInfo) -> ApplicationInfo:
+        return ApplicationInfo(special_case.value, '', '')
+
+    def __handle_regular_case(self, data_item: Dict[str, Dict]) -> ApplicationInfo:
+        raw_app_info, = data_item.items()
+        app_name, app_info = raw_app_info
+
+        wm_name = app_info[WM_NAME_KEY] if WM_NAME_KEY in app_info else ''
+        tab = app_info[TAB_KEY] if TAB_KEY in app_info else ''
+
+        if not wm_name and not tab:
+            raise ValueError(f'Application [{app_name}] has empty wm_name and tab!')
+
+        return ApplicationInfo(app_name, wm_name, tab)
+
     def try_read(self, data: List) -> List[ApplicationInfo]:
         if not isinstance(data, list):
             raise ValueError(f'Incorrect data type [{type(data)}]!')
@@ -28,26 +43,17 @@ class ApplicationInfoReader:
         result = []
 
         for data_item in data:
-            if data_item in SpecialApplicationInfo.list():
-                special_case = data_item
+            is_special_case = data_item in SpecialApplicationInfo.list()
+
+            if is_special_case:
                 has_other_application_infos = len(data) > 1
+                special_case = SpecialApplicationInfo(data_item)
 
                 if has_other_application_infos:
-                    raise ValueError(f'Special case [{special_case}] must be the only one in a list!')
+                    raise ValueError(f'Special case [{special_case.value}] must be the only one in a list!')
 
-                app_info = ApplicationInfo(special_case, '', '')
-                result.append(app_info)
-
-            raw_app_info, = data_item.items()
-            app_name, app_info = raw_app_info
-
-            wm_name = app_info[WM_NAME_KEY] if WM_NAME_KEY in app_info else ''
-            tab = app_info[TAB_KEY] if TAB_KEY in app_info else ''
-
-            if not wm_name and not tab:
-                raise ValueError(f'Application [{app_name}] has empty wm_name and tab!')
-
-            app_info = ApplicationInfo(app_name, wm_name, tab)
-            result.append(app_info)
+                result.append(self.__handle_special_case(special_case))
+            else:
+                result.append(self.__handle_regular_case(data_item))
 
         return result
