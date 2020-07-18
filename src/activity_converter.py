d3f5aa8cc4067415fc95a3bac06ab5b303736987
Alena Bartosh <alena.mathematics@gmail.com>
2020-07-18 21:50:51 +0300

    ✅ ActivityConverter: add unittests and implement from_string method
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>

--- src/activity_converter.py
+++ src/activity_converter.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+
 from activity import Activity
+from bool_parser import BoolParser
 
 
 class ActivityConverter:
+    EXPECTED_COLUMNS_NUMBER = 6
+
     @staticmethod
     def to_string(activity: Activity) -> str:
         return f'{activity.start_time}\t' \
@@ -13,4 +18,21 @@ class ActivityConverter:
 
     @staticmethod
     def from_string(value: str) -> Activity:
-        raise NotImplementedError()
+        if value[-1] != '\n':
+            raise ValueError(f'String [{value}] should be ended with a new line!')
+
+        columns = value.split(sep='\t')
+
+        if len(columns) != ActivityConverter.EXPECTED_COLUMNS_NUMBER:
+            raise ValueError(f'Unexpected columns number: [{len(columns)}] '
+                             f'!= {ActivityConverter.EXPECTED_COLUMNS_NUMBER} in string [{value}]!')
+
+        try:
+            start_time, end_time, activity_time, wm_class, window_name, is_work_time = columns
+            start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S.%f')
+            end_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S.%f')
+            is_work_time = BoolParser.parse(is_work_time.replace('\n', ''))
+        except Exception as e:
+            raise ValueError(f'Incorrect string [{value}]! {e}')
+
+        return Activity(wm_class, window_name, start_time, is_work_time).set_end_time(end_time)
--- tests/test_ActivityConverter.py
+++ tests/test_ActivityConverter.py
@@ -1,5 +1,5 @@
 import unittest
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from activity import Activity
 from activity_converter import ActivityConverter
@@ -29,9 +29,41 @@ class ActivityConverterTestCase(unittest.TestCase):
             with self.subTest(name=sub_test):
                 self.assertEqual(expected, ActivityConverter.to_string(activity))
 
-    def test_convert_from_string(self):
-        with self.assertRaises(NotImplementedError):
-            ActivityConverter.from_string('In progress ;)')
+    def test_convert_from_string_when_correct_activity_line(self):
+        sub_tests_data = [(
+            '2020-07-18 20:00:00.000001\t2020-07-18 20:30:00.000001\t0:30:00.000000\twm_class1\twindow_name1\tFalse\n',
+            Activity('wm_class1',
+                     'window_name1',
+                     datetime(2020, 7, 18, 20, 0, 0, 1),
+                     is_work_time=False).set_end_time(datetime(2020, 7, 18, 20, 30, 0, 1))
+        )]
+
+        for i, (line, expected) in enumerate(sub_tests_data, 1):
+            with self.subTest(name=f'Subtest №{i}'):
+                self.assertEqual(expected, ActivityConverter.from_string(line))
+
+    def test_convert_from_string_when_incorrect_activity_line(self):
+        sub_tests_data = [(
+            '2020-07-18 20:00:00.000001\t2020-07-18 20:30:00.000001\t0:30:00.000000\twm_class1\twindow_name1\tFALSEE\n',
+            'Incorrect string \\['
+            '2020-07-18 20:00:00.000001\t2020-07-18 20:30:00.000001\t0:30:00.000000\twm_class1\twindow_name1\tFALSEE\n'
+            '\\]! Unexpected value \\[FALSEE\\]'
+        ), (
+            '2020-07-18 20:00:00.000001\t2020-07-18 20:30:00.000001\t0:30:00.000000\twm_class1\twindow_name1\tFalse',
+            'String \\['
+            '2020-07-18 20:00:00.000001\t2020-07-18 20:30:00.000001\t0:30:00.000000\twm_class1\twindow_name1\tFalse'
+            '\\] should be ended with a new line!'
+        ), (
+            '2020-07-18 20:00:00.000001\t2020-07-18 20:30:00.000001\t0:30:00.000000\twm_class1\twindow_name1\n',
+            'Unexpected columns number: \\[5\\] != 6 in string \\['
+            '2020-07-18 20:00:00.000001\t2020-07-18 20:30:00.000001\t0:30:00.000000\twm_class1\twindow_name1\n\\]!'
+        )]
+
+        for i, (line, expected_msg) in enumerate(sub_tests_data, 1):
+            with self.subTest(name=f'Subtest №{i}'):
+                with self.assertRaisesRegex(ValueError,
+                                            expected_regex=expected_msg):
+                    ActivityConverter.from_string(line)
 
 
 if __name__ == '__main__':
