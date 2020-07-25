9c23f5dec1a769d6b4b614e7dbb723a0a259458b
Alena Bartosh <alena.mathematics@gmail.com>
2020-07-25 19:04:15 +0300

    ✅ ActivityWriter: test_when_record_first_activities_of_the_day
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>

--- tests/test_ActivityWriter.py
+++ tests/test_ActivityWriter.py
@@ -1,7 +1,7 @@
 import unittest
 from datetime import date, datetime
 from pathlib import Path
-from unittest.mock import patch, mock_open
+from unittest.mock import call, patch, mock_open
 
 from activity import Activity
 from activity_writer import ActivityWriter
@@ -19,16 +19,33 @@ class ActivityWriterTestCase(unittest.TestCase):
     @patch('builtins.open', new_callable=mock_open)
     @patch('time_provider.TimeProvider.today', return_value=date(2020, 7, 21))
     @patch('pathlib.Path.is_dir', return_value=True)
-    def test_when_record_first_activity(self, mock_is_dir_res, mock_today_res, mock_open_res) -> None:
+    def test_when_record_first_activities_of_the_day(self, mock_is_dir_res, mock_today_res, mock_open_res) -> None:
         writer = ActivityWriter(TimeProvider(), Path('/output_dir/'), '{date}.tsv')
         writer.write(self.activity)
 
         mock_is_dir_res.assert_called_once()
         mock_today_res.assert_called_once()
         mock_open_res.assert_called_once_with('/output_dir/2020-07-21.tsv', 'a')
-        mock_open_res().write.assert_called_once_with(
+        mock_open_res.return_value.write.assert_called_once_with(
             '2020-07-21 20:30:00.000001\t2020-07-21 21:30:00.000002\t1:00:00.000001\twm_class1\twindow_name1\tTrue\n')
-        mock_open_res().flush.assert_called_once()
+        mock_open_res.return_value.flush.assert_called_once()
+
+        second_activity = Activity('wm_class2',
+                                   'window_name2',
+                                   datetime(2020, 7, 21, 22, 30, 0, 2),
+                                   is_work_time=True).set_end_time(datetime(2020, 7, 21, 22, 30, 0, 8))
+
+        writer.write(second_activity)
+
+        mock_is_dir_res.assert_called_once()
+        self.assertEqual(2, mock_today_res.call_count)
+        mock_open_res.assert_called_once_with('/output_dir/2020-07-21.tsv', 'a')
+        self.assertEqual(2, mock_open_res.return_value.write.call_count)
+        self.assertEqual(
+            call('2020-07-21 22:30:00.000002\t2020-07-21 22:30:00.000008\t'
+                 '0:00:00.000006\twm_class2\twindow_name2\tTrue\n'),
+            mock_open_res.return_value.write.call_args)
+        self.assertEqual(2, mock_open_res.return_value.flush.call_count)
 
     @patch('pathlib.Path.is_dir', return_value=True)
     def test_when_date_not_in_file_mask(self, mock_is_dir_res) -> None:
@@ -66,8 +83,8 @@ class ActivityWriterTestCase(unittest.TestCase):
 
         mock_today_res.assert_not_called()
         mock_open_res.assert_not_called()
-        mock_open_res().write.assert_not_called()
-        mock_open_res().flush.assert_not_called()
+        mock_open_res.return_value.write.assert_not_called()
+        mock_open_res.return_value.flush.assert_not_called()
 
     @patch('builtins.open', return_value=None)
     @patch('time_provider.TimeProvider.today', return_value=date(2020, 7, 21))
