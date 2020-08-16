c3de2e56532a03846d70d2dab1c70fe3cc8a1926
Alena Bartosh <alena.mathematics@gmail.com>
2020-08-16 17:36:49 +0300

    ✅ DatetimeHelper: fix test names
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>

--- tests/test_DatetimeHelper.py
+++ tests/test_DatetimeHelper.py
@@ -6,7 +6,7 @@ from datetime_helper import DatetimeHelper
 
 class DatetimeHelperTestCase(unittest.TestCase):
 
-    def test_when_correct_value_passed(self):
+    def test_when_get_dates_between_correct_dates(self):
         for (start, end, expected) in [
             (date(2020, 10, 27), date(2020, 10, 27), [date(2020, 10, 27)]),
             (date(2020, 10, 27), date(2020, 10, 28), [date(2020, 10, 27), date(2020, 10, 28)]),
@@ -14,7 +14,7 @@ class DatetimeHelperTestCase(unittest.TestCase):
         ]:
             self.assertEqual(expected, DatetimeHelper.get_dates_between(start, end))
 
-    def test_when_incorrect_value_passed(self):
+    def test_when_get_dates_between_incorrect_dates(self):
         with self.assertRaisesRegex(ValueError,
                                     expected_regex='End date \\[2020-07-20\\] should be greater '
                                                    'than start date \\[2020-07-21\\]!'):
