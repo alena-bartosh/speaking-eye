165c317e1c5b30517f34d8b126f4a3ad9382f613
Alena Bartosh <alena.mathematics@gmail.com>
2020-08-29 19:41:32 +0300

    🚧 Implement ActivityReader
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>

---
+++ src/activity_reader.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from typing import List
+import logging
+
+from activity import Activity
+from activity_converter import ActivityConverter
+
+
+class ActivityReader:
+
+    def __init__(self, logger: logging.Logger) -> None:
+        self.logger = logger
+
+    def read(self, raw_data_file: Path) -> List[Activity]:
+        activities = []
+
+        if not raw_data_file.exists():
+            self.logger.debug(f'File with raw data [{raw_data_file}] does not exist for this day')
+
+            return activities
+
+        with open(str(raw_data_file)) as file:
+            for line in file:
+                activity = ActivityConverter.from_string(line)
+
+                activities.append(activity)
+
+        return activities
