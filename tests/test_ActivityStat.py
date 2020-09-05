d13b8c5c3472a67d42e0af9654bf150e94e3bbc9
Alena Bartosh <alena.mathematics@gmail.com>
2020-09-05 14:22:34 +0300

    ✅ ActivityStat: test_when_update_time
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>

--- tests/test_ActivityStat.py
+++ tests/test_ActivityStat.py
@@ -1,5 +1,5 @@
 import unittest
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from activity import Activity
 from activity_stat import ActivityStat
@@ -31,3 +31,30 @@ class ActivityStatTestCase(unittest.TestCase):
                 expected_regex='Activity \\[2020-07-21 20:30:00\tNone\tNone\twm_class1\twindow_name1\tTrue\n\\] '
                                'should be finished before updating ActivityStat!'):
             ActivityStat(self.activity).update(not_finished_activity)
+
+    def test_when_update_time(self) -> None:
+        not_working_activity = Activity('wm_class1',
+                                        'window_name1',
+                                        datetime(2020, 7, 21, 21, 30, 0, 3),
+                                        is_work_time=False).set_end_time(datetime(2020, 7, 21, 22, 30, 0, 3))
+        working_activity = Activity('wm_class1',
+                                    'window_name1',
+                                    datetime(2020, 7, 21, 22, 30, 0, 4),
+                                    is_work_time=True).set_end_time(datetime(2020, 7, 21, 22, 35, 0, 5))
+
+        sub_tests_data = {
+            'Update off time stat when it was empty':
+                (self.activity, not_working_activity, timedelta(0, 3600, 1), timedelta(0, 3600)),
+            'Update work time stat when it was empty':
+                (not_working_activity, working_activity, timedelta(0, 300, 1), timedelta(0, 3600)),
+            'Update work time stat when it was not empty':
+                (self.activity, working_activity, timedelta(0, 3900, 2), timedelta(0)),
+        }
+
+        for sub_test, (activity, same_activity_with_diff_time, work_time, off_time) in sub_tests_data.items():
+            with self.subTest(name=sub_test):
+                activity_stat = ActivityStat(activity)
+                activity_stat.update(same_activity_with_diff_time)
+
+                self.assertEqual(activity_stat.work_time, work_time)
+                self.assertEqual(activity_stat.off_time, off_time)
