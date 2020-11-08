d457f5b77bd2b222eaea2a02909977bd7447da30
Alena Bartosh <alena.mathematics@gmail.com>
2020-11-08 15:20:45 +0200

    🏷️ ApplicationInfo.__eq__(): respect Liskov substitution principle
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>

--- src/application_info.py
+++ src/application_info.py
@@ -6,7 +6,7 @@ class ApplicationInfo:
         self.tab_re = tab_re
         self.is_distracting = is_distracting
 
-    def __eq__(self, other: 'ApplicationInfo') -> bool:
+    def __eq__(self, other: object) -> bool:
         """
         Overrides the default implementation
         to use the object values instead of identifiers for comparison
