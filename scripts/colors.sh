0489b8002e9d37e40b8f867b040ae296d1f882a4
Alena Bartosh <alena.mathematics@gmail.com>
2021-01-02 17:15:42 +0200

    🔨 Fix colors.sh for CI
    Signed-off-by: Alena Bartosh <alena.mathematics@gmail.com>

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# NOTE: we need to update TERM env variable since we use colors more than [0-7]
+#       based on https://serverfault.com/a/524057
+export TERM=xterm-256color
+
 DARK_GREY=$(tput setab 8)
 GREEN=$(tput setaf 10)
 RED=$(tput setaf 1)
