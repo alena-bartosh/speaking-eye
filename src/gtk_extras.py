a1b8b3d1b517edefb9ea240709a6d5c34155293c
Alena Bartosh <51479635+alena-bartosh@users.noreply.github.com>
2021-06-28 23:51:59 +0300

    🏷️️ gtk_extras: strict typing

--- src/gtk_extras.py
+++ src/gtk_extras.py
@@ -1,5 +1,7 @@
+from typing import Optional, cast
+
 from gi.repository import Gtk
 
 
-def get_window_name(window: Gtk.Window) -> str:
-    return window.get_name() if window else 'EMPTY WINDOW'
+def get_window_name(window: Optional[Gtk.Window]) -> str:
+    return cast(str, window.get_name()) if window else 'EMPTY WINDOW'
