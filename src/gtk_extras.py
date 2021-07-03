5e995beddb7971988688a4426d85f94426a9c315
Alena Bartosh <51479635+alena-bartosh@users.noreply.github.com>
2021-07-03 17:47:53 +0300

    📝 gtk_extras: Add docstring

--- src/gtk_extras.py
+++ src/gtk_extras.py
@@ -4,4 +4,5 @@ from gi.repository import Gtk
 
 
 def get_window_name(window: Optional[Gtk.Window]) -> str:
+    """Get window name using GTK or label for empty otherwise"""
     return cast(str, window.get_name()) if window else 'EMPTY WINDOW'
